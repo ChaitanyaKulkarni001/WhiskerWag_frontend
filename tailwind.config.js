3b67ab261420f9541517550c3438c70ad8cea88a
Chaitanya_Kulkarni <kulkarnichaitanya001@gmail.com>
2024-09-21 22:36:06 +0530

    Added api url

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  darkMode : "class",
+  theme: {
+    extend: {
+
+      colors: {
+        'dark-blue': '#003366',  // Replace with your desired dark blue color
+        'dark-cyan': '#004d4d',  // Replace with your desired dark cyan color
+      },
+      backgroundImage: {
+        'instagram-gradient': 'linear-gradient(135deg, #f58529, #feda75, #dd2a7b, #8134af)', // Define your gradient
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+}
+
